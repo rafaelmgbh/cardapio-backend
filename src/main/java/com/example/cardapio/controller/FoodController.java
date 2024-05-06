48ad7732495cff7e9e0a8d7fc7c41d5bea147be1
Rafael Santos <rafael.santos@voraztecnologia.com>
2024-05-06 16:19:06 -0300

    [FEAT] added food entity .

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,33 @@
+package com.example.cardapio.controller;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/foods")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository foodRepository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        foodRepository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll() {
+        List<FoodResponseDTO> foodList = foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
